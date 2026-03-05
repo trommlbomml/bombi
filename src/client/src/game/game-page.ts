69f447aecbc161e0e3c05684a245736e8c622be2
trommlbomml <p.friedland@gmx.de>
2026-03-05 21:58:50 +0100

    loading phazer game on entering and destroy on leave

--- src/client/src/game/game-page.ts
+++ src/client/src/game/game-page.ts
@@ -1,6 +1,7 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import {appStore} from "../instrumentation/app-state.ts";
+import BootScene from "./boot-scene.ts";
 
 @customElement('game-page')
 export class GamePage extends LitElement {
@@ -8,33 +9,48 @@ export class GamePage extends LitElement {
     isVisible;
 
     private unsubscribe?: () => void;
+    private game?: Phaser.Game;
 
     constructor() {
         super();
         this.isVisible = false;
     }
 
-
-    connectedCallback() {
-        super.connectedCallback();
+    firstUpdated(): void {
         this.unsubscribe = appStore.subscribe(state => {
-            this.isVisible = state.state === 'InGame';
+            const newIsVisible = state.state === 'InGame';
+            if (this.isVisible !== newIsVisible) {
+                this.isVisible = newIsVisible;
+                if (this.isVisible) {
+                    const config: Phaser.Types.Core.GameConfig = {
+                        type: Phaser.AUTO,
+                        parent: "game",
+                        width: 800,
+                        height: 600,
+                        backgroundColor: "#1d1d1d",
+                        scene: [BootScene]
+                    }
+
+                    this.game = new Phaser.Game(config);
+                } else {
+                    this.game?.destroy(false);
+                    this.game = undefined;
+                }
+            }
         });
     }
 
     disconnectedCallback() {
         this.unsubscribe?.();
         super.disconnectedCallback();
+        this.game?.destroy(false);
+        this.game = undefined;
     }
 
     render() {
-        if (this.isVisible) {
-            return html`
-                <div class="card">
-                    blubb
-                </div>
-    `;
-        }
+        return html`
+            <div id="game"></div>
+        `;
     }
     protected createRenderRoot() {
         return this;
